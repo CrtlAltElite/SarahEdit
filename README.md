9148af9e6f474a4b25b7407b44a21597a0566ee3
Sarah-Stodder <sjsarahboo@gmail.com>
2022-08-13 14:41:30 -0600

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+## Description
+Welcome to my weather app! This is a simple JavaScript application that gives you current weather information for the city you search for.
+
+
+
+### Executing program
+To use this application, all you need to do is search for a city.
+If you would like to clone this application, you dont have any additional installations required.  
+Please note, Axios is used with the script tag and not an npm install.
+* Navigate into the folder you where you want to place this application
+* `git clone https://github.com/Sarah-Stodder/Weather-JS.git`
+* `cd Weather-JS`
+* now you are inside the project!
+
+
+## Authors
+
+Contributors names and contact info
+
+Sarah Stodder
+Github: https://github.com/Sarah-Stodder
---
+++ weather.js
@@ -0,0 +1,79 @@
+
+
+const KEY = "68232441021bf3bde59a8e821d928075";
+
+const getWeather = async(city,key) => {
+    try {
+   const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${KEY}`)
+   console.log(response.data);
+   showWeather(response.data);
+  
+} catch (error) {
+    console.error(error);
+  }
+};
+
+//getWeather('canon', KEY)
+
+
+function showWeather(data){
+    let high = data.main.temp_max
+    let low = data.main.temp_min
+    let forcast = data.weather[0].main
+    let humidity = data.main.humidity
+    let icon= data.weather[0].icon
+    console.log(icon)
+   
+    document.getElementById("high").innerHTML=high + ' &#176;F'
+    document.getElementById("low").innerHTML=low + ' &#176;F'
+    document.getElementById("forcast").innerHTML=forcast
+    document.getElementById("humidity").innerHTML=humidity+ "%"
+    document.getElementById("icon").innerHTML=`<img src=http://openweathermap.org/img/wn/${icon}@2x.png>`
+   // iconImg.src = iconUrl;
+
+}
+
+
+const form = document.querySelector('#form')
+form.addEventListener('submit', (event) => {
+    console.log("here i am")
+    event.preventDefault();
+    let city = document.querySelector('#city');
+    let key = KEY
+    console.log(city.value);
+    getWeather(city.value,key)
+});
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours(); 
+    let m = date.getMinutes(); 
+    let s = date.getSeconds();
+    let mm = date.getMonth(); 
+    let dd = date.getDate();
+    let year = date.getFullYear();
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    let time = h + ":" + m + ":" + s + " " + session;
+    let dates = "Todays date: "+ mm+"/"+dd+"/"+year + " ";
+    document.getElementById("MyClockDisplay").innerText = time + dates;
+    document.getElementById("MyClockDisplay").textContent = dates +"  "+ time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
